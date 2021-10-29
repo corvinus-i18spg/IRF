22b65cb5a0db5aa30b5d41edb5269fc2304018d2
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-10-29 08:31:35 +0200

    xml

--- UserMaintenance/week6/Form1.cs
+++ UserMaintenance/week6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week6.Entities;
 using week6.MnbServiceReference;
 
@@ -33,8 +34,26 @@ namespace week6
             var result = response.GetExchangeRatesResult;
 
             dataGridView1.DataSource = Rates;
-        }
 
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
 
     }
 }
