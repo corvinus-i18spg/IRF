b6dbad683440951408d4c87f4517d1b9f76c7efe
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-10-29 08:57:59 +0200

    currency

--- UserMaintenance/week6/Form1.Designer.cs
+++ UserMaintenance/week6/Form1.Designer.cs
@@ -29,9 +29,9 @@ namespace week6
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
@@ -53,16 +53,16 @@ namespace week6
             // 
             // chartRateData
             // 
-            chartArea5.Name = "ChartArea1";
-            this.chartRateData.ChartAreas.Add(chartArea5);
-            legend5.Name = "Legend1";
-            this.chartRateData.Legends.Add(legend5);
+            chartArea6.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend6);
             this.chartRateData.Location = new System.Drawing.Point(450, 62);
             this.chartRateData.Name = "chartRateData";
-            series5.ChartArea = "ChartArea1";
-            series5.Legend = "Legend1";
-            series5.Name = "Series1";
-            this.chartRateData.Series.Add(series5);
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chartRateData.Series.Add(series6);
             this.chartRateData.Size = new System.Drawing.Size(338, 300);
             this.chartRateData.TabIndex = 1;
             this.chartRateData.Text = "chart1";
--- UserMaintenance/week6/Form1.cs
+++ UserMaintenance/week6/Form1.cs
@@ -17,6 +17,7 @@ namespace week6
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<String> Currencies = new BindingList<string>();
 
         public Form1()
         {
@@ -35,6 +36,7 @@ namespace week6
             var result = response.GetExchangeRatesResult;
 
             dataGridView1.DataSource = Rates;
+            comboBox1.DataSource = Currencies;
 
             var xml = new XmlDocument();
             xml.LoadXml(result);
@@ -58,8 +60,46 @@ namespace week6
             }
 
             DataToChart();
+            GetCurrencies();
         }
 
+        private void GetCurrencies()
+        {
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                Currencies = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+
+
+            }
+
+        }
 
         private void DataToChart()
         {
