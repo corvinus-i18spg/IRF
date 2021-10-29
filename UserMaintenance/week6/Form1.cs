842e33d1dfa0e29b9e24f43e5440f73d6e12d7d4
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-10-29 08:37:44 +0200

    datachart

--- UserMaintenance/week6/Form1.cs
+++ UserMaintenance/week6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week6.Entities;
 using week6.MnbServiceReference;
@@ -52,8 +53,31 @@ namespace week6
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
+
+
             }
+
+            DataToChart();
         }
 
+        private void DataToChart()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
     }
+
 }
