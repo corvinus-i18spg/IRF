f18fb807cd2dacfdeda4a3b2cefe600f8870efb4
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-11-12 21:28:39 +0100

    png

--- UserMaintenance/mintak/Entities/Car.cs
+++ UserMaintenance/mintak/Entities/Car.cs
@@ -1,13 +1,18 @@
 ﻿using mintak.Abstraction;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace mintak.Entities
 {
-    class Car: Toy
+    class Car : Toy
     {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
