a87a6b3f8134edfc1390789ee63a2c6e82b35f6a
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-12-03 14:13:56 +0100

    birth

---
+++ micro_sim/micro_sim/Entities/Person/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace micro_sim.Entities.Person
+{
+    class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NumberOfKids { get; set; }
+        public double Probability { get; set; }
+
+    }
+}
