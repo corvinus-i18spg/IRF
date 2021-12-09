69421b06b7b9a04f895d98b3e57ce94f7b1629b3
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2021-12-09 16:13:53 +0100

    gc

--- gamer/Form1.cs
+++ gamer/Form1.cs
@@ -7,11 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace gamer
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
